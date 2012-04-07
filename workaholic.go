9445b25b67f67760ed18ef7494ba46636829c201
Joubin Houshyar <alphazero@sensesay.net>
2012-04-07 01:19:38 -0400

    REFACTOR - WIP - be more idiomatic - BUILDS

--- workaholic.go
+++ workaholic.go
@@ -1,127 +1,188 @@
-// Workaholic is a light-weight, in-memory, actor framework.  The name of the package is intended to
-// be semantically informative: this is not an executor framework and the tasks are coarse grained.
-// For example, a typical task may be "process all incoming service x requests". /end hand waving
-//
-//
-// TODO: doc this
+// Workaholic is a light-weight treatment to provide command & control semantics
+// for long running goroutines aka workers. Each worker is a simple FSM exposing
+// command and control input channels and a very basic instrumentation (status)
+// output channels.
+
 package workaholic
 
 import (
+	"log" // TEMP
 )
 
-//type TaskContext map[string]interface{}
-type TaskContext struct {
-	parent  *TaskContext
-	bindings map[string]interface{}
-	ichan <-chan bool
-}
+// ----------------------------------------------------------------------------
+// Tasks
+// ----------------------------------------------------------------------------
 
-// A task defines the signature of a contextual function.
-// Both input arguments (for the task) and any side-effecting
-// and/or results are also bound to the task context.  The details
-// are task specifics.
-//
-// Workers are dumb and simply invoke tasks and are not concerned about
-// the result of the work.  But they do have 2 distinct concerns when
-// running tasks:
-//
-// 1 - the task signals a fault.  A fault indicates that progress can no
-// longer be made.  Worker pauses.
-//
-// 2 - the task signals completion.  Job is done and worker pauses.
-//
-// 3 - the task was interrupted (if applicable).
-//
+// A Task is a function
+// REVU: review this (contexts, names instead of functions .. etc.)
+type Task func()
 
-type taskStatus int8
-
-type Task func(context TaskContext) (interrupt_code, error)
-
-type interrupt_code int8
-
+type control_code int8
 const (
-	_ interrupt_code = iota // Note: fsm depends on 0 here.
-	work
-	pause
-	report
-	quit
+	_ control_code = iota // Note: fsm depends on 0 here.
+	Work
+	Pause
+	Report
+	Quit
 )
-var Interrupts = [...]interrupt_code { work, pause, report, quit}
+/* used by tests */
+var Interrupts = [...]control_code { Work, Pause, Report, Quit}
+/* used by tests */
 
-type status_code int8
 
+type Status_code int8
 const (
-	_ status_code = iota - 2
+	_ Status_code = iota - 3
 	faulted
 	interrupted
-	ready
-	working
-	done
+	idle
+	busy
+	terminated
 )
-var ErrorStatusCodes = [...]status_code {faulted, interrupted, ready, working, done}
+func (s Status_code) String() string {
+	switch s {
+	case faulted:
+		return "faulted"
+	case interrupted:
+		return "interrupted"
+	}
+	return "not-coded"
+}
+/* used by tests */
+var criticalStats = 2
+var StatusCodes = [...]Status_code {faulted, interrupted, idle, busy}
+/* used by tests */
 
-// control channels convey interrupt_code (signals) from clients to workers.
+// Status_code channels convey Status_code (signals) from worker to clients.
 // define general, write only, and, real-only types
 
-type controlCh chan interrupt_code
-type controlIn <-chan interrupt_code
-type Control chan<- interrupt_code
+//type statusCh chan Status_code
+type statusOut chan<- Status_code
 
-// status_code channels convey status_code (signals) from worker to clients.
-// define general, write only, and, real-only types
+// ----------------------------------------------------------------------------
+// Worker
+// ----------------------------------------------------------------------------
 
-type statusCh chan status_code
-type statusOut chan<- status_code
-type Status <-chan status_code
+type Command chan<- Task
+type Control chan<- control_code
+type Status <-chan Status_code
+type Faults <-chan interface{}
 
 // worker struct is a handle for a worker
 type Worker struct {
-	/* pkg assigned */
-	id        int
-	name      string
-	controlCh controlCh
-	statusCh  statusCh
-	/* user assigned */
-	task    Task
-	context TaskContext
+
+	/* identity */
+
+	Id        int
+	Name      string
+
+	/* internals */
+
+	commandCh chan Task
+	statusCh  chan Status_code
+	controlCh chan control_code
+	fsmstat Status_code
+
 	/* ui */
+
+	// Command is a buffered channel of Task
+	// used to issue work for worker
+	Command Command
+
+	// Control is a len 1 write only channel of control_code
+	// used to issue interrupts to the worker
 	Control Control
+
+	// Status is a len 1 read only channel of Status_code
+	// used by worker to send status to user
 	Status  Status
 }
 
-// REVU: in general, add timed variants for blocking calls
-
-func (w *Worker) Work() {
-	w.controlCh <- work
-}
-
-func (w *Worker) Pause() {
-	w.controlCh <- pause
-}
-
-func (w *Worker) Report() {
-	w.controlCh <- work
-}
-
-func (w *Worker) Stop() {
-	w.controlCh <- quit
-}
-
-/* TODO:
- * - setup the worker go fms
- * - worker init and return
- */
-func NewWorker(name string, id int, context TaskContext, task Task) *Worker {
+func NewWorker(name string, id int, qlen int) *Worker {
 	w := new(Worker)
 	// initialize
-	w.controlCh = make(controlCh)
-	w.Control = (chan<- interrupt_code)(w.controlCh)
-	w.statusCh = make(statusCh)
-	w.Status = (<-chan status_code)(w.statusCh)
-	w.task = task
-	w.context = context
+	w.commandCh = make(chan Task, qlen)
+	w.Command = (chan<- Task)(w.commandCh)
 
-//	go w.fsm()
+	w.controlCh = make(chan control_code, 1)
+	w.Control = (chan<- control_code)(w.controlCh)
+
+	w.statusCh = make(chan Status_code, 1)         // REVU: the 1 bothers me .. a bit
+	w.Status = (<-chan Status_code)(w.statusCh)
+
+	go w.fsm()
 
 	return w
+}
+
+func (w *Worker) fsm()  {
+
+	var controller = (<-chan control_code)(w.controlCh)
+	var commander = (<-chan Task)(w.commandCh)
+    var statout = (chan<- Status_code)(w.statusCh)
+
+	w.fsmstat = idle // in this state only at startup and after a Pause command
+
+await_signal:
+	interrupt := <-controller
+
+interrupted:
+	w.fsmstat = interrupted
+
+	switch interrupt {
+	case Work:
+		goto Work
+	case Pause:
+		goto Pause
+	case Report:
+		goto Report
+	case Quit:
+		goto shutdown
+	}
+panic("SHOULD NOT BE REACHED")
+
+Work:
+	w.fsmstat = busy
+
+	select {
+	case interrupt = <-controller:
+		goto interrupted
+	case task := <-commander:
+		w.perform(task)
+		goto Work
+	}
+
+Pause:
+	w.fsmstat = idle
+
+	goto await_signal
+
+Report:
+	// TODO: do the Report
+	// REVU: do we even need this?
+	goto await_signal
+
+//faulted:
+////	w.statusCh <- workerStatus{id, fault, taskstatus, &controller}
+//	w.statusCh <- faulted
+//	goto await_signal
+
+shutdown:
+	w.fsmstat = terminated
+
+	statout<- terminated
+	log.Println("shutdown ..")
+	// TODO: add shutdown hook for worker
+}
+
+func (w *Worker) perform(task Task) {
+	defer func() {
+		if p := recover(); p != nil {
+		// REVU: get rid of status and use faults
+//			w.faultsCh <- p
+			w.statusCh <- faulted
+		}
+	}()
+
+	task()
 }
--- workaholic_test.go
+++ workaholic_test.go
@@ -10,21 +10,33 @@ import (
 
 func TestConstantValueAssumptions_interrupt_code (t *testing.T) {
 	for i, code := range Interrupts {
-		if !acceptableInterruptCode (code) {
+		if code <= 0  {
 			t.Fatalf("Interrupts[%d]=%d is not an acceptable interrupt_code",i, code)
 		}
 	}
 }
-func acceptableInterruptCode (c interrupt_code) bool {
-	return c > 0
-}
+//func acceptableInterruptCode (c interrupt_code) bool {
+//	return c > 0
+//}
+
 func TestConstantValueAssumptions_status_code (t *testing.T) {
+	errmsg := [...]string{
+		"StatusCodes[%d]=%d is not an acceptable critical status_code",
+		"StatusCodes[%d]=%d is not an acceptable noncritical status_code",
+	}
+	var stype int
 	for i, code := range StatusCodes {
-		if !acceptableInterruptCode (code) {
-			t.Fatalf("Interrupts[%d]=%d is not an acceptable interrupt_code",i, code)
+		acceptable := true
+		switch {
+		case i < criticalStats:
+			stype = 0
+			acceptable = code < 0
+		default:
+			stype = 1
+			acceptable = code >= 0
+		}
+		if !acceptable {
+			t.Fatalf(errmsg[stype], i, code)
 		}
 	}
-}
-func acceptableStatusCode (c interrupt_code) bool {
-	return c > 0
 }
